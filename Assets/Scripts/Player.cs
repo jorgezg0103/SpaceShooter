ae86396d788bc1b882ee2f8c139e88fc493c7ed9
Jorge <jorgezg0103@gmail.com>
2024-02-12 20:32:54 +0100

    Added Shoot function to Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,26 @@ public class Player : MonoBehaviour
 {
     private int _health = 4;
     [SerializeField] float _speed = 3;
+    private PoolController _poolController;
+
+    private float _bulletOffset = 0.6f;
+
+    private void Awake() {
+        _poolController = GameObject.Find("Spawner").GetComponent<PoolController>();
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
+    }
 
     private void FixedUpdate() {
         MovePlayerToMousePosition();
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.tag == "Bullet") {
+        if(collision.gameObject.tag == "EnemyBullet") {
             _health--;
             if(_health <= 0) {
                 Debug.Log("Game Over");
@@ -26,6 +39,12 @@ public class Player : MonoBehaviour
     private void MovePlayerToMousePosition() {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = Vector2.MoveTowards(transform.position, mousePosition, _speed * Time.deltaTime);
+    }
+
+    private void Shoot() {
+        GameObject newBullet = _poolController.GetBullet();
+        newBullet.GetComponent<Proyectile>().SetPlayerBullet();
+        newBullet.transform.position = transform.position + new Vector3(0, _bulletOffset, 0);
     }
 
 }
